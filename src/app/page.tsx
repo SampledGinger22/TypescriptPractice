92fc6f754d09861b44153f4c720d7b7dededd77b
SampledGinger22 <mason.britsch@gmail.com>
2025-04-02 14:16:10 -0500

    Formatted display results, updated tailwind, attempted to resolve tailwind import issues, fixed search bar, formatted phone number displays, followed db initialization instructions. 1.25 hour mark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import AdvocateDTO from "./api/dto/AdvocateDTO";
 
@@ -45,46 +44,55 @@ export default function Home() {
     setFilteredAdvocates(advocates);
   };
 
+  const formatPhoneNumber = (phoneNumber: string) => {
+    if (phoneNumber.length === 10) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6)}`;
+    } else if (phoneNumber.length === 7) {
+      return `${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3)}`;
+    }
+    return phoneNumber;
+  }
+
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
+    <main className="m-6">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-center">Solace Advocates</h1>
+      <br/>
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-fit w-50%">
+        <p className="text-lg font-semibold text-black">Search</p>
+        <input className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Searching for:" onChange={onChange} />
+        <button className="ml-2 px-4 py-2 bg-blue-500 text-white rounded" onClick={onClick}>
+          Reset Search
+        </button>
+      </form>
       <br />
       <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
-      </div>
-      <br />
-      <br />
-      <table>
+      <table className="min-w-full border-collapse shadow-md">
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr className="bg-gray-200">
+            <th className="border p-2 text-left w-min">First Name</th>
+            <th className="border p-2 text-left">Last Name</th>
+            <th className="border p-2 text-left">City</th>
+            <th className="border p-2 text-left">Degree</th>
+            <th className="border p-2 text-left">Specialties</th>
+            <th className="border p-2 text-left">Years of Experience</th>
+            <th className="border p-2 text-left">Phone Number</th>
+          </tr>
         </thead>
-        <tbody>
+        <tbody className="align-top">
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr>
-                <td>{advocate.firstName ?? null}</td>
-                <td>{advocate.lastName ?? null}</td>
-                <td>{advocate.city ?? null}</td>
-                <td>{advocate.degree ?? null}</td>
-                <td>
-                  {advocate.specialties && advocate.specialties.length > 0 ? (
-                    advocate.specialties.map((s) => <div>{s}</div>)
-                  ) : null}
+              <tr key={advocate.phoneNumber} className="even:bg-gray-100">
+                <td className="border p-2">{advocate.firstName ?? null}</td>
+                <td className="border p-2">{advocate.lastName ?? null}</td>
+                <td className="border p-2">{advocate.city ?? null}</td>
+                <td className="border p-2">{advocate.degree ?? null}</td>
+                <td className="border p-2 h-min overflow-auto">
+                  {advocate.specialties && advocate.specialties.length > 0
+                    ? advocate.specialties.map((s, index) => <div key={index}>{s}</div>)
+                    : null}
                 </td>
-                <td>{advocate.yearsOfExperience ?? null}</td>
-                <td>{advocate.phoneNumber ?? null}</td>
+                <td className="border p-2">{advocate.yearsOfExperience ?? null}</td>
+                <td className="border p-2">{ advocate.phoneNumber ? formatPhoneNumber(advocate.phoneNumber.toString()) :  null}</td>
               </tr>
             );
           })}
